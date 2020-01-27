e86a810a32ed004b7e58332ca188eeb39e022ff9
Guo Yunhe <i@guoyunhe.me>
2020-01-27 15:57:38 +0200

    PUBLIC_URL fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
           <Route path="/">
