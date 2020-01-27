e6feda6f2008ba92cb7983fe6fae759401e93f68
guoyunhe <i@guoyunhe.me>
2020-01-27 13:59:35 +0000

    Deploying to gh-pages from  - 1c0ba1b39f739548f4e28b505ad6450f9d53d596 🚀

---
+++ precache-manifest.8970adc957db538a7a24febe879500fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73c0caadc8b0293e83e1c2a1b505a26",
+    "url": "https://guoyunhe.github.io/yes-we-can/index.html"
+  },
+  {
+    "revision": "38a5278ee0294f1bb5e5",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/css/main.312befd5.chunk.css"
+  },
+  {
+    "revision": "d759bd2684ca101afe87",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/js/2.fb5f4aa2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/js/2.fb5f4aa2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38a5278ee0294f1bb5e5",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/js/main.a3d529c8.chunk.js"
+  },
+  {
+    "revision": "0066647eeee314815831",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/js/runtime-main.e626dc16.js"
+  },
+  {
+    "revision": "13976c55d974aedd54144afed5d07a04",
+    "url": "https://guoyunhe.github.io/yes-we-can/static/media/photo-1.13976c55.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6916fcfc176b03a17252cd7fd40b5f5.js"
+  "https://guoyunhe.github.io/yes-we-can/precache-manifest.8970adc957db538a7a24febe879500fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://guoyunhe.github.io/yes-we-can/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
